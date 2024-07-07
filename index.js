c5f4d5cca469174281452132bfd03b72bc68ebec
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-07 23:31:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
 const chalk = require("chalk");
-const { execSync } = require("child_process");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,7 +19,8 @@ app.use((req, res, next) => {
 
   res.on("finish", () => {
     const duration = Date.now() - start;
-    const statusColor = res.statusCode >= 200 && res.statusCode < 300 ? "green" : "red";
+    const statusColor =
+      res.statusCode >= 200 && res.statusCode < 300 ? "green" : "red";
 
     console.log(
       chalk.bold(
@@ -69,49 +69,15 @@ const loadRoutes = (directory) => {
   });
 };
 
-const installMissingModules = () => {
-  const packageJsonPath = path.join(__dirname, "package.json");
-  const packageLockPath = path.join(__dirname, "package-lock.json");
-
-  if (!fs.existsSync(packageJsonPath) || !fs.existsSync(packageLockPath)) {
-    console.error(
-      chalk.bold.red(
-        "Error: No package.json or package-lock.json found."
-      )
-    );
-    process.exit(1);
-  }
-
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-  const missingModules = [];
-
-  for (const dependency in packageJson.dependencies) {
-    try {
-      require.resolve(dependency);
-    } catch (error) {
-      missingModules.push(dependency);
-    }
-  }
-
-  if (missingModules.length > 0) {
-    console.log(chalk.bold.green("Installing missing modules..."));
-    deployRoute("npm modules", "green");
-    execSync(`npm install ${missingModules.join(" ")}`);
-    console.log(chalk.bold.green("Modules installed successfully."));
-  } else {
-    console.log(chalk.bold.green("No missing modules found."));
-  }
-};
+// Serve login.html at the path /login
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 console.log(chalk.bold.cyan("Deploying routes..."));
 loadRoutes(path.join(__dirname, "routes"));
 console.log(chalk.bold.cyan("Routes deployment complete."));
 
-console.log(chalk.bold.cyan("Checking and installing missing modules..."));
-installMissingModules();
-console.log(chalk.bold.cyan("Module installation complete."));
-
 app.listen(port, () => {
   console.log(chalk.bold(`Server is running on http://localhost:${port}`));
 });
-
