df7228d2be753b2ae7e324d3afa2ae64b84910bf
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-08 00:02:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const { execSync } = require("child_process");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-
+// Custom middleware
 app.use((req, res, next) => {
   const start = Date.now();
-  const ipAddress = req.ip || req.connection.remoteAddress;
+  const ipAddress = req.socket.remoteAddress || req.connection.remoteAddress;
   const currentDate = new Date().toISOString();
 
   res.on("finish", () => {
@@ -26,23 +27,17 @@ app.use((req, res, next) => {
         `Request on ${chalk.cyanBright(req.path)} took ${duration} ms - Status: ${chalk[statusColor](res.statusCode)} - IP: ${ipAddress} - Date: ${currentDate}`
       )
     );
-
-    userData.push({
-      path: req.path,
-      duration: duration,
-      status: res.statusCode,
-      ip: ipAddress,
-      date: currentDate,
-    });
   });
 
   next();
 });
 
+// Function to deploy routes
 const deployRoute = (routeName, color) => {
   console.log(chalk.bold[color](`DEPLOYED ROUTE [${routeName}]`));
 };
 
+// Function to load a single route
 const loadRoute = (filePath) => {
   try {
     const route = require(filePath);
@@ -55,6 +50,7 @@ const loadRoute = (filePath) => {
   }
 };
 
+// Function to load all routes in a directory
 const loadRoutes = (directory) => {
   fs.readdirSync(directory).forEach((file) => {
     const filePath = path.join(directory, file);
@@ -62,6 +58,7 @@ const loadRoutes = (directory) => {
   });
 };
 
+// Function to install missing modules
 const installMissingModules = () => {
   const packageJsonPath = path.join(__dirname, "package.json");
   const packageLockPath = path.join(__dirname, "package-lock.json");
@@ -92,14 +89,17 @@ const installMissingModules = () => {
   }
 };
 
+// Deploy routes
 console.log(chalk.bold.cyan("Deploying routes..."));
 loadRoutes(path.join(__dirname, "routes"));
 console.log(chalk.bold.cyan("Routes deployment complete."));
 
+// Install missing modules
 console.log(chalk.bold.cyan("Checking and installing missing modules..."));
 installMissingModules();
 console.log(chalk.bold.cyan("Module installation complete."));
 
+// Start the server
 app.listen(port, () => {
   console.log(chalk.bold(`Server is running on http://localhost:${port}`));
 });
