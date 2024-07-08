93af476cb781cdb7af7d38679641c7e471cb34a0
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-08 17:56:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const axios = require('axios').default; // Menggunakan axios sebagai pengganti got
+const got = require('axios').default; // Menggunakan axios sebagai pengganti got
 const app = express();
 const PORT = process.env.PORT || 3000;
 
