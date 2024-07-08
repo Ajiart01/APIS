4d7da7764914171193f8eea1ea778de3c7ea5cad
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-07-08 17:33:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
